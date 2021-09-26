be462db0d6c6bbae439c8c049f112c71fb3d6ed5
jaeheng <jaeheng@126.com>
2021-09-26 16:37:23 +0800

    gh-pages

---
+++ assets/layout.140a1368.js
@@ -0,0 +1,1 @@
+import{r as t,o as e,d as r,a}from"./vendor.db6eb6ad.js";const o={computed:{title(){return this.$route.meta.title||"Bnb-Mobile"}}},n={class:"layout"};o.render=function(o,i,l,s,u,d){const b=t("bnb-navbar"),c=t("router-view");return e(),r("div",n,[a(b,{title:d.title},null,8,["title"]),a(c)])};export{o as default};
